e8538350e63113792eccf484077cf02ebf544efa
bithadder <86196266+bithadder@users.noreply.github.com>
2021-07-21 13:33:22 +0700

    Update antinodes-lambo.sh

--- antinodes-lambo.sh
+++ antinodes-lambo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Keeping free of hddcoin nodes...
 while true; do
-for i in ~/littlelambocoin/venv/bin/littlelambocoin show -c | grep '28444\|6899\|8444\|15554\|9444' | awk '{ print $4 }' | sed 's/\.\.\.//'; do ~/littlelambocoin/venv/bin/littlelambocoin show -r $i;done
+for i in ~/littlelambocoin/venv/bin/littlelambocoin show -c | grep '8433\|8440\|28444\|6899\|8444\|15554\|9444' | awk '{ print $4 }' | sed 's/\.\.\.//'; do ~/littlelambocoin/venv/bin/littlelambocoin show -r $i;done
 sleep 10
 done
 
